d90f937ca8e3d1c4533a57fec07023f978e40054
Varun <varunvadda99@gmail.com>
2022-07-10 14:43:06 +0530

    static pentacle completed

--- src/component/pentacle.jsx
+++ src/component/pentacle.jsx
@@ -1,11 +1,13 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./pentacle.css";
 
 let currentrow = 0;
 let currenttail = 0;
 let isgameover = false;
+let wordle = "SUPER";
+
 const Pentacle = () => {
   const keys = [
     "Q",
@@ -46,6 +48,7 @@ const Pentacle = () => {
     ["", "", "", "", ""],
   ]);
   const [message, setMessage] = useState("");
+  const [enable, setEnable] = useState([0, 0, 0, 0, 0, 0]);
 
   const handlekeystroke = (key) => {
     if (key !== "ENTER" && key !== "«" && currenttail < 5 && currentrow < 6) {
@@ -53,10 +56,6 @@ const Pentacle = () => {
       temp[currentrow][currenttail] = key;
       setGuessbox(temp);
       currenttail++;
-      //   if (currenttail > 4) {
-      //     currentrow = currentrow + 1;
-      //     currenttail = 0;
-      //   }
     } else if (key === "«" && currenttail >= 0) {
       let temp = [...guessbox];
       temp[currentrow][currenttail - 1] = "";
@@ -64,8 +63,11 @@ const Pentacle = () => {
       currenttail--;
     } else if (key === "ENTER") {
       if (currenttail > 4) {
+        const inter = [...enable];
+        inter[currentrow] = 1;
+        setEnable(inter);
         const guess = guessbox[currentrow].join("");
-        if (guess === "SUPER") {
+        if (guess === wordle) {
           showmessage("You Done it");
           isgameover = true;
         } else {
@@ -96,13 +98,20 @@ const Pentacle = () => {
       <div className="messageContainer">
         {message.length > 0 ? <p>{message}</p> : <></>}
       </div>
-      <div className="matrixContainer">
+      <div className="matrixContainer ">
         {guessbox.map((guessrow, i) => (
           <div id={"guessrow" + i}>
             {guessrow.map((guess, j) => (
               <div
-                className="tile"
-                data={guess}
+                className={
+                  enable[i] === 1
+                    ? wordle[j] === guess
+                      ? "tile green-overlay"
+                      : wordle.includes(guess)
+                      ? "tile yellow-overlay"
+                      : "tile grey-overlay"
+                    : "tile"
+                }
                 id={"guessrow" + i + "guesstail" + j}
               >
                 {guess}
